a22f053fe6bfd7d069c1d275d0cac3277b5bf5e5
Jennifer Arevalo Morales <jennifer.arevalom@gmail.com>
2018-11-22 19:56:17 -0800

    Add files via upload

---
+++ modelos-preentrenados/perrosygatos/scripts/cnn_executor.py
@@ -0,0 +1,19 @@
+#-----------------------------------------------------------
+# Cargando modelo de disco
+#-----------------------------------------------------------
+import tensorflow as tf
+from keras.models import model_from_json
+import matplotlib.pyplot as plt
+
+def cargarModelo():
+    print("Cargando modelo desde el disco ...")
+    json_file = open('../modelo/jenn_cnn.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("../modelo/jenn_cnn.h5")
+    loaded_model.compile(optimizer='adam',decay=0.0, loss='binary_crossentropy', metrics=['accuracy'])
+    print("Modelo cargado de disco!")
+    graph = tf.get_default_graph()
+    return loaded_model, graph
